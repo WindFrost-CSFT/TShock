c6201ff1fc43adab08cfce5fc9533716cc88f79f
Cai <13110818005@qq.com>
2026-01-29 23:32:12 +0800

    Add miss NPCAddBuffTimeMax

--- TShockAPI/Bouncer.cs
+++ TShockAPI/Bouncer.cs
@@ -2978,42 +2978,35 @@ namespace TShockAPI
 		// Moved to Projectile.StatusNPC(int i).
 		private static Dictionary<int, short> NPCAddBuffTimeMax = new Dictionary<int, short>()
 		{
-			{ BuffID.Poisoned, 3600 },              // BuffID: 20
-			{ BuffID.OnFire, 1200 },                // BuffID: 24
-			{ BuffID.Confused, short.MaxValue },    // BuffID: 31 Brain of Confusion Internal Item ID: 3223
-			{ BuffID.CursedInferno, 600 },          // BuffID: 39
-			{ BuffID.Frostburn, 900 },              // BuffID: 44
-			{ BuffID.Ichor, 1200 },                 // BuffID: 69
-			{ BuffID.Venom, 1800 },                 // BuffID: 70
-			{ BuffID.Midas, 120 },                  // BuffID: 72
-			{ BuffID.Wet, 1500 },                   // BuffID: 103
-			{ BuffID.Lovestruck, 1800 },            // BuffID: 119
-			{ BuffID.Stinky, 1800 },                // BuffID: 120
-			{ BuffID.Slimed, 1500 },                // BuffID: 137
-			{ BuffID.SoulDrain, 30 },               // BuffID: 151
-			{ BuffID.ShadowFlame, 660 },            // BuffID: 153
-			{ BuffID.DryadsWard, 120 },             // BuffID: 165
-			{ BuffID.BoneJavelin, 900 },            // BuffID: 169
-			{ BuffID.StardustMinionBleed, 900 },    // BuffID: 183
-			{ BuffID.DryadsWardDebuff, 120 },       // BuffID: 186
-			{ BuffID.Daybreak, 300 },               // BuffID: 189 Solar Eruption Item ID: 3473, Daybreak Item ID: 3543
-			{ BuffID.BetsysCurse, 600 },            // BuffID: 203
-			{ BuffID.Oiled, 540 },                  // BuffID: 204
-			//{ BuffID.BlandWhipEnemyDebuff, 240  },  // BuffID: 307
-			//{ BuffID.SwordWhipNPCDebuff, 240  },    // BuffID: 309
-			{ BuffID.ScytheWhipEnemyDebuff, 240  }, // BuffID: 310
-			//{ BuffID.FlameWhipEnemyDebuff, 240  },  // BuffID: 313
-			// { BuffID.ThornWhipNPCDebuff, 240  },    // BuffID: 315
-			// { BuffID.RainbowWhipNPCDebuff, 240  },  // BuffID: 316
-			// { BuffID.MaceWhipNPCDebuff, 240  },     // BuffID: 319
-			{ BuffID.GelBalloonBuff, 1800  },       // BuffID: 320
-			{ BuffID.OnFire3, 1200 },               // BuffID: 323
-			{ BuffID.Frostburn2, 1200 },            // BuffID: 324
-			//{ BuffID.BoneWhipNPCDebuff, 240 },      // BuffID: 326
-			{ BuffID.TentacleSpike, 540 },          // BuffID: 337
-			//{ BuffID.CoolWhipNPCDebuff, 240 },      // BuffID: 340
-			{ BuffID.BloodButcherer, 540 },         // BuffID: 344
-			{ BuffID.Shimmer, 100 },		        // BuffID: 353
+			{ BuffID.Venom, 660 },                     // BuffID: 70
+			{ BuffID.CursedInferno, 420 },             // BuffID: 39
+			{ BuffID.OnFire, 600 },                    // BuffID: 24
+			{ BuffID.Ichor, 1200 },                    // BuffID: 69
+			{ BuffID.Confused, 240 },                  // BuffID: 31
+			{ BuffID.Poisoned, 1800 },                 // BuffID: 20
+			{ BuffID.Midas, 120 },                     // BuffID: 72
+			{ BuffID.OnFire3, 1200 },                  // BuffID: 323
+			{ BuffID.Frostburn2, 960 },                // BuffID: 324
+			{ BuffID.Bleeding, 420 },                  // BuffID: 30
+			{ BuffID.Stinky, 300 },                    // BuffID: 120
+			{ BuffID.Slimed, 180 },                    // BuffID: 137
+			{ BuffID.Hemorrhage, 780 },                // BuffID: 375
+			{ BuffID.BrokenArmor, 1260 },              // BuffID: 36
+			{ BuffID.BoneJavelin, 900 },               // BuffID: 169
+			{ BuffID.Daybreak, 300 },                  // BuffID: 189
+			{ BuffID.TentacleSpike, 540 },             // BuffID: 337
+			{ BuffID.BloodButcherer, 540 },            // BuffID: 344
+			{ BuffID.BetsysCurse, 600 },               // BuffID: 203
+			{ BuffID.StardustMinionBleed, 900 },       // BuffID: 183
+			{ BuffID.ShadowFlame, 480 },               // BuffID: 153
+			{ BuffID.SoulDrain, 30 },                  // BuffID: 151
+			{ BuffID.Oiled, 540 },                     // BuffID: 204
+			{ BuffID.Frostburn, 180 },                 // BuffID: 44
+			{ BuffID.EelWhipNPCDebuff, 240 },          // BuffID: 362
+			{ BuffID.ScytheWhipEnemyDebuff, 240 },     // BuffID: 310
+			{ BuffID.DryadsWardDebuff, 120 },          // BuffID: 186
+			{ BuffID.Wet, 60 },                        // BuffID: 103
+			{ BuffID.DryadsWard, 120 },                // BuffID: 165
 		};
 
 		/// <summary>
