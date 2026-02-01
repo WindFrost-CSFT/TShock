ca4940e1262d754414837ed2a261307b0d42f771
Cai <13110818005@qq.com>
2026-02-01 23:26:41 +0800

    Increse the threshold of ItemDrop range check

--- TShockAPI/Bouncer.cs
+++ TShockAPI/Bouncer.cs
@@ -1193,7 +1193,7 @@ namespace TShockAPI
 				return;
 			}
 
-			if (!args.Player.IsInRange((int)(pos.X / 16f), (int)(pos.Y / 16f)))
+			if (!args.Player.IsInRange((int)(pos.X / 16f), (int)(pos.Y / 16f), 64))
 			{
 				TShock.Log.ConsoleDebug(GetString("Bouncer / OnItemDrop rejected from range check from {0}", args.Player.Name));
 				args.Player.SendData(PacketTypes.ItemDrop, "", id);
