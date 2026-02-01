2ca13549f3ff758e87a1d7a9ef14a0748b4c2561
Cai <13110818005@qq.com>
2026-02-01 21:01:49 +0800

    Add last time for AddBuff kick infomation

--- TShockAPI/Bouncer.cs
+++ TShockAPI/Bouncer.cs
@@ -2161,7 +2161,7 @@ namespace TShockAPI
 				if (detectedNPCBuffTimeCheat)
 				{
 					TShock.Log.ConsoleDebug(GetString("Bouncer / OnNPCAddBuff rejected abnormal buff ({0}, last for {4}) added to {1} ({2}) from {3}.", type, npc.TypeName, npc.type, args.Player.Name, time));
-					args.Player.Kick($"Added buff {type} to {npc.TypeName} NPC abnormally.", true);
+					args.Player.Kick($"Added buff {type} (last for {time}) to {npc.TypeName} NPC abnormally.", true);
 					args.Handled = true;
 				}
 			}
